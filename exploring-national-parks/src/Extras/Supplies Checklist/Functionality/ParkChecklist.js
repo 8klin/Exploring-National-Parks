8f78307da74f7e768d6364e637a72534d91fa344
Helen <tui55736@temple.edu>
2026-03-13 02:35:29 -0400

    updated style location

--- exploring-national-parks/src/Extras/Supplies Checklist/Functionality/ParkChecklist.js
+++ exploring-national-parks/src/Extras/Supplies Checklist/Functionality/ParkChecklist.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
-import "./parkChecklist.css";
+import "../Style/parkChecklist.css";
 
 const starterItems = [
   { id: 1, name: "Water bottle", category: "Essentials", checked: true },
