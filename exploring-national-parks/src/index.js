e194998a373657e8fe1230e9a6089af2d7a8a0b1
Kristy <tul41776@temple.edu>
2026-03-13 04:27:15 -0400

    fixed compatibility with existing code

--- exploring-national-parks/src/index.js
+++ exploring-national-parks/src/index.js
@@ -21,6 +21,7 @@ import HomePage from './HomePage.js';
 import ParkPlan from './ParkPlan.js';
 import TempleLocationsPage from './Extras/TempleLocations/Components/locationsPage.jsx';
 import ParkChecklist from "./Extras/SuppliesChecklist/Functionality/ParkChecklist.js";
+import TempleNewsPage from './Extras/TempleNews/Components/TempleNewsPage.jsx';
 import Navbar from './GlobalComponents/Navbar.jsx';
 import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import Footer from './GlobalComponents/Footer.jsx';
@@ -38,6 +39,7 @@ root.render(
                 <Route path="/ParkPlan" element={<ParkPlan />} />
                 <Route path="/TempleLocationsPage" element={<TempleLocationsPage />} />
                 <Route path="/ParkChecklist" element={<ParkChecklist />} />
+                <Route path="/TempleNewsPage" element={<TempleNewsPage />} />
             </Routes>
             <Footer></Footer>
         </Router>
